dec056ab721edd0c83b91aa008f856b41b63ecc1
Cyber Knight <cyberknight755@gmail.com>
2021-10-02 03:26:23 +0800

    drm/atomic: nuke logging
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2262,9 +2262,6 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 		} else if (kp_active_mode() == 3) {
 			cpu_input_boost_kick_max(50);
 			devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
-		} else {
-			/* Do nothing :) */
-			pr_info("Battery profile detected! Skipping CPU & DDR bus boosts\n");
 		}
 	}
 
