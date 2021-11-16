9c38ea3fdc61876c72b64cbe3883eeb062679ba4
Cyber Knight <cyberknight755@gmail.com>
2021-11-16 22:45:56 +0800

    kernel: nuke logging
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2257,9 +2257,6 @@ long _do_fork(unsigned long clone_flags,
 	} else if (task_is_zygote(current) && (kp_active_mode() == 3)) {
 		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
-	} else if (task_is_zygote(current) && (kp_active_mode() == 1)) {
-		/* Do nothing :) */
-		pr_info("Battery profile detected! Skipping CPU & DDR bus boost...\n");
 	}
 
 	/*
